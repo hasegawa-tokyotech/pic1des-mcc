86081b8128df5bc0ef203721e2f521042655c5e6
Jun Hasegawa <hasegawa.j.aa@m.titech.ac.jp>
2020-07-17 14:40:10 +0900

    Initial Commit

---
+++ pic1des+mcc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pic1des+mcc
+//
+//  Created by Jun on 2020/07/17.
+//  Copyright © 2020 Tokyo Tech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
